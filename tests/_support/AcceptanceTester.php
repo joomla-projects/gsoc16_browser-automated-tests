7a73da251a61b13f400d32b26ffd382a3daf7860
javier gomez <javier.gomez.corio@gmail.com>
2016-05-12 11:42:25 +0200

    Add acceptance tester

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -20,7 +20,31 @@ class AcceptanceTester extends \Codeception\Actor
 {
     use _generated\AcceptanceTesterActions;
 
-   /**
-    * Define custom actions here
-    */
+    /**
+     * @Given Joomla CMS is installed
+     */
+    public function joomlaCMSIsInstalled()
+    {
+        // throw new \Codeception\Exception\Incomplete("Step `Joomla CMS is installed` is not defined");
+    }
+
+    /**
+     * @When Login into Joomla administrator with username :arg1 and password :arg1
+     */
+    public function loginIntoJoomlaAdministratorWithUsernameAndPassword($username, $password)
+    {
+        $I = $this;
+        $I->amOnPage('administrator/');
+        $I->fillField('Username', $username);
+        $I->fillField(['id' => 'mod-login-password'], $password);
+        $I->click('Log in');
+    }
+
+    /**
+     * @Then I see administrator dashboard
+     */
+    public function iSeeAdministratorDashboard()
+    {
+        // throw new \Codeception\Exception\Incomplete("Step `I see administrator dashboard` is not defined");
+    }
 }
