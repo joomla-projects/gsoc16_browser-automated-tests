657a26ef2fcb1e4361231c56f8429fe2a2923ec8
puneet0191 <puneet.0191@gmail.com>
2016-06-15 23:00:29 +0530

    Updating Login Step Class File

--- tests/_support/Step/Acceptance/Joomla3/Administrator/LoginSteps.php
+++ tests/_support/Step/Acceptance/Joomla3/Administrator/LoginSteps.php
@@ -1,21 +1,11 @@
 <?php
 
-namespace Step\Acceptance\Administrator;
-use Page\Acceptance\Joomla3\Administrator\Login;
+namespace Step\Acceptance\Joomla3\Administrator;
+
+use Page\Acceptance\Joomla3\Administrator\LoginPage;
 
 class LoginSteps extends \AcceptanceTester
 {
-
-	private $usernameFieldId = ['id' => 'mod-login-username'];
-
-	private $passwordFieldId = ['id' => 'mod-login-password'];
-
-	private $pageTitleClass = ['class' => 'page-title'];
-
-	private $loginButtonPath = ['xpath' => "//button[contains(normalize-space(), 'Log in')]"];
-
-	private $loginPageURL = "/administrator/index.php";
-
 	/**
 	 * @When Login into Joomla administrator with username :arg1 and password :arg1
 	 */
@@ -23,16 +13,16 @@ class LoginSteps extends \AcceptanceTester
 	{
 		$I = $this;
 		$I->amGoingTo('I open Joomla Administrator Login Page');
-		$I->amOnPage($this->loginPageURL);
-		$I->waitForElement($this->usernameFieldId, 60);
+		$I->amOnPage(LoginPage::$pageURL);
+		$I->waitForElement(LoginPage::$usernameField, 60);
 		$I->amGoingTo('Fill Username Text Field');
-		$I->fillField($this->usernameFieldId, $arg1);
+		$I->fillField(LoginPage::$usernameField, $arg1);
 		$I->amGoingTo('Fill Password Text Field');
-		$I->fillField($this->passwordFieldId, $arg2);
+		$I->fillField(LoginPage::$passwordField, $arg2);
 
-		$I->amGoingTo('I click Login button');
-		$I->click($this->loginButtonPath);
+		$I->amGoingTo('I click LoginPage button');
+		$I->click(LoginPage::$loginButton);
 		$I->amGoingTo('I wait to see Administrator Control Panel');
-		$I->waitForText('Control Panel', 4, $this->pageTitleClass);
+		$I->waitForText(LoginPage::$controlPanelText, 10, LoginPage::$pageTitle);
 	}
 }
