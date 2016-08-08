83f70709e935d4bdd650f0581f93b79b228053e0
Prital Patel <baldhapiyu@gmail.com>
2016-08-08 14:41:08 +0530

    [code style] Adding code style improvement in acceptance tester class

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -7,7 +7,6 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
-use Page\Acceptance\Administrator\LoginPage;
 use Page\Acceptance\Administrator\AdminPage;
 
 /**
@@ -55,7 +54,7 @@ class AcceptanceTester extends \Codeception\Actor
 	 *
 	 * @param   string  $page  Optional, if not given checks will be done in the current page
 	 *
-	 * @note:   doAdminLogin() before
+	 * @note    :   doAdminLogin() before
 	 * @since   3.7
 	 *
 	 * @return  void
@@ -81,7 +80,9 @@ class AcceptanceTester extends \Codeception\Actor
 	/**
 	 * Function to select Toolbar buttons in Joomla! Admin Toolbar Panel
 	 *
-	 * @param   string $button The full name of the button
+	 * @param   string  $button  The full name of the button
+	 *
+	 * @since   3.7
 	 *
 	 * @return  void
 	 */
@@ -162,11 +163,14 @@ class AcceptanceTester extends \Codeception\Actor
 	 *
 	 * Note: We recommend use of checkAllResults function only after searchForItem to be sure you are selecting only the desired result set
 	 *
-	 * @return void
+	 * @since   3.7
+	 *
+	 * @return  void
 	 */
 	public function checkAllResults()
 	{
 		$I = $this;
+
 		$I->comment("Selecting Checkall button");
 		$I->click(['xpath' => "//thead//input[@name='checkall-toggle' or @name='toggle']"]);
 	}
@@ -174,71 +178,88 @@ class AcceptanceTester extends \Codeception\Actor
 	/**
 	 * Selects an option in a Chosen Selector based on its id
 	 *
-	 * @param   string $selectId The id of the <select> element
-	 * @param   string $option   The text in the <option> to be selected in the chosen selector
+	 * @param   string  $selectId  The id of the <select> element
+	 * @param   string  $option    The text in the <option> to be selected in the chosen selector
 	 *
-	 * @return void
+	 * @since   3.7
+	 *
+	 * @return  void
 	 */
 	public function selectOptionInChosenById($selectId, $option)
 	{
 		$chosenSelectID = $selectId . '_chzn';
+
 		$I = $this;
 		$I->comment("I open the $chosenSelectID chosen selector");
 		$I->click(['xpath' => "//div[@id='$chosenSelectID']/a/div/b"]);
 		$I->comment("I select $option");
 		$I->click(['xpath' => "//div[@id='$chosenSelectID']//li[text()='$option']"]);
-		$I->wait(1); // Gives time to chosen to close
+
+		// Gives time to chosen to close
+		$I->wait(1);
 	}
+
 	/**
 	 * Function to Verify the Tabs on a Joomla! screen
 	 *
-	 * @param  Array $expectedTabs Expected Tabs on the Page
-	 * @param  Mixed $tabsLocator  Locator for the Tabs in Edit View
+	 * @param   array  $expectedTabs  Expected Tabs on the Page
+	 * @param   Mixed  $tabsLocator   Locator for the Tabs in Edit View
 	 *
-	 * @return void
+	 * @since   3.7
+	 *
+	 * @return  void
 	 */
 	public function verifyAvailableTabs($expectedTabs, $tabsLocator = ['xpath' => "//ul[@id='myTabTabs']/li/a"])
 	{
 		$I = $this;
+
 		$actualArrayOfTabs = $I->grabMultiple($tabsLocator);
-		$I->comment("Fetch the current list of Tabs in the edit view which is: " . implode(", ", $actualArrayOfTabs));
-		$url = $I->grabFromCurrentUrl();
-		$I->assertEquals($expectedTabs, $actualArrayOfTabs, "Tab Labels do not match on edit view of" . $url);
+
+		$I->comment(
+			"Fetch the current list of Tabs in the edit view which is: " . implode(", ", $actualArrayOfTabs)
+		);
+
+		$I->assertEquals(
+			$expectedTabs,
+			$actualArrayOfTabs, "Tab Labels do not match on edit view of" . $I->grabFromCurrentUrl()
+		);
+
 		$I->comment('Verify the Tabs');
 	}
 
 	/**
 	 * Function to Logout from Administrator Panel in Joomla!
 	 *
-	 * @return void
+	 * @since   3.7
+	 *
+	 * @return  void
 	 */
 	public function doAdministratorLogout()
 	{
 		$I = $this;
-		$I->click(['xpath' => "//ul[@class='nav nav-user pull-right']//li//a[@class='dropdown-toggle']"]);
-		$I->comment("I click on Top Right corner toggle to Logout from Admin");
-		$I->waitForElement(['xpath' => "//li[@class='dropdown open']/ul[@class='dropdown-menu']//a[text() = 'Logout']"], TIMEOUT);
-		$I->click(['xpath' => "//li[@class='dropdown open']/ul[@class='dropdown-menu']//a[text() = 'Logout']"]);
-		$I->waitForElement(['id' => 'mod-login-username'], TIMEOUT);
-		$I->waitForText('Log in', TIMEOUT, ['xpath' => "//fieldset[@class='loginform']//button"]);
+		$I->click(
+			['xpath' => "//ul[@class='nav nav-user pull-right']//li//a[@class='dropdown-toggle']"]
+		);
 
+		$I->comment("I click on Top Right corner toggle to Logout from Admin");
+		$I->waitForElement(
+			['xpath' => "//li[@class='dropdown open']/ul[@class='dropdown-menu']//a[text() = 'Logout']"],
+			TIMEOUT
+		);
+
+		$I->click(
+			['xpath' => "//li[@class='dropdown open']/ul[@class='dropdown-menu']//a[text() = 'Logout']"]
+		);
+
+		$I->waitForElement(['id' => 'mod-login-username'], TIMEOUT);
+		$I->waitForText(
+			'Log in',
+			TIMEOUT,
+			['xpath' => "//fieldset[@class='loginform']//button"]
+		);
 	}
 
 	/**
-	 * Selects an option in a Joomla Radio Field based on its label
-	 *
-	 * @return void
-	 */
-	public function selectOptionInRadioField($label, $option)
-	{
-		$I = $this;
-		$I->comment("Trying to select the $option from the $label");
-		$label   = $webDriver->findField(['xpath' => "//label[contains(normalize-space(string(.)), '" . $label . "')]"]);
-		$radioId = $label->getAttribute('for');
-		$I->click(['xpath' => "//fieldset[@id='$radioId']/label[contains(normalize-space(string(.)), '$option')]"]);
-	}
-
-    /**
      * Prepare menu item creation by choosing menu and adding title.
      *
      * @param   string  $title  The Menu Item title
