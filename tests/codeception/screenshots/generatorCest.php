6168b7d46b27d41f7cd05924742b316ba5bbc153
Shivam Rajput <shivamdiehard@gmail.com>
2017-07-31 21:10:43 +0530

    Image Name automation and URL of different Views

--- tests/codeception/screenshots/generatorCest.php
+++ tests/codeception/screenshots/generatorCest.php
@@ -3,50 +3,107 @@
 define("JPATH_SCREENSHOTS", "../../../../screenshots/");
 
 
+/**
+ * Simple test runner for screenshots for the marketing team
+ */
 class generatorCest
 {
 	protected $urlConfig;
 
 	protected $adminSuites;
 
-	protected $adminFrontendSuites;
+	protected $frontendAdminSuites;
 
 	protected $frontendSuites;
 
+	protected $baseDir;
+
 	public function _before(ScreenshotsTester $I)
 	{
+		$this->baseDir = dirname(dirname(dirname(__DIR__))) . '/screenshots/';
+
 		$this->urlConfig = Symfony\Component\Yaml\Yaml::parse(file_get_contents('tests/codeception/screenshots/screenshot.urls.yml'));
 
 		// @todo improve
 		$this->adminSuites = $this->urlConfig['Administrator Backend'];
-		$this->adminFrontendSuites = $this->urlConfig['Administrator Frontend'];
+		$this->frontendAdminSuites = $this->urlConfig['Administrator Frontend'];
 		$this->frontendSuites = $this->urlConfig['Guest'];
+
+		$this->version = $this->urlConfig['Version'];
 	}
 
 	public function _after(ScreenshotsTester $I)
 	{
 	}
 
-	// tests
+	/**
+	 * Screenshots of the installation
+	 *
+	 * @param ScreenshotsTester $I
+	 */
 	public function installationScreenshots(ScreenshotsTester $I)
 	{
 		$I->comment('I open Joomla Installation Configuration Page');
 
-		$I->installJoomlaRemovingInstallationFolder();
+		// Comment these lines for not DOING THE INSTALLATION
+		/*$I->installJoomlaRemovingInstallationFolder();
+		$I->doAdministratorLogin();
+		$I->disableStatistics();*/
 	}
 
+	/**
+	 * Screenshots of the admin area
+	 *
+	 * @param ScreenshotsTester $I
+	 */
 	public function administratorScreenshots(ScreenshotsTester $I)
 	{
 		$I->amOnPage('administrator/');
 		$I->doAdministratorLogin();
 
 		// @todo Improve
-		$target = dirname(dirname(dirname(__DIR__))) . "/screenshots/";
+		$target = $this->baseDir . 'administrator/';
 
-		$this->makeScreenshots($I, $this->adminSuites, $target);
+		$this->makeScreenshots($I, $this->adminSuites, $target, 'administrator/');
 	}
 
-	protected function makeScreenshots($I, $suites, $target)
+	/**
+	 * Screenshots of the frontend as logged in admin
+	 *
+	 * @param ScreenshotsTester $I
+	 */
+	public function frontendAdministratorScreenshots(ScreenshotsTester $I)
+	{
+		$I->amOnPage('index.php');
+		$I->doFrontEndLogin();
+
+		$target = $this->baseDir . 'frontendAdmin/';
+
+		$this->makeScreenshots($I, $this->frontendAdminSuites, $target, 'frontendAdmin/');
+	}
+
+	/**
+	 * Frontend Screenshots as guest
+	 *
+	 * @param ScreenshotsTester $I
+	 */
+	public function frontendScreenshots(ScreenshotsTester $I)
+	{
+		$I->amOnPage('index.php');
+
+		$target = $this->baseDir . 'frontend/';
+
+		$this->makeScreenshots($I, $this->frontendSuites, $target, 'frontend/');
+	}
+
+	/**
+	 * Create screenshots
+	 *
+	 * @param $I
+	 * @param $suites
+	 * @param $target
+	 */
+	protected function makeScreenshots($I, $suites, $target, $base = '')
 	{
 		foreach ($suites as $suite)
 		{
@@ -55,24 +112,51 @@ class generatorCest
 
 			$outputFolder = $target . $folder;
 
-			$I->comment('Output ' . $outputFolder);
-
 			if (!is_dir($outputFolder))
 			{
 				mkdir($outputFolder, 0777, true);
 			}
 
+$I->comment('Write something in the terminal ' + print_r($urls, true));
 			foreach ($urls as $url => $fileName)
 			{
 				$I->comment('URL ' . $url);
 				$I->comment('Filename ' . $fileName);
+				parse_str(parse_url($url, PHP_URL_QUERY), $params);
+				if($params['option'])
+				{
+					$par = explode("_",$params['option']);
+					$par = ucfirst($par[1]);
+					$fil = 'Help4x-Component-' . $par;
+					$I->comment($fil);
 
+					$I->comment($params['option']);
+					
+					if(isset($params['view']))
+					{
+						$fil = $fil . '-' . ucfirst($params['view']);
+											
+						if(isset($params['layout']))
+						{ 
+							$fil = $fil . '-' . ucfirst($params['layout']);
+					
+						}
+
+					}
+					$fil = $fil . '-en';
+					$I->comment('Final ' . $fil);
+
+				}
 				$parts = explode("/", $fileName);
 
 				if (!is_dir($outputFolder))
 				{
 					mkdir($outputFolder, 0777, true);
 				}
+
+				// @todo Get the filename and add Help-3x (whatever) to it
+				// because we currently filename is edit/filename
+
 
 				// @todo improve
 				if (count($parts) > 1)
@@ -89,8 +173,21 @@ class generatorCest
 
 				$I->amOnPage($url);
 
+				// Tab support
+				$tabs = explode('#', $url);
+
+				if (count($tabs) > 1)
+				{
+					$tab = $tabs[count($tabs) - 1];
+
+					$I->comment('Selecting tab ' . $tab);
+
+					$I->wait(1);
+					$I->click('a[href="#' . $tab . '"]');
+				}
+
 				// @todo improve
-				$I->makeScreenshot(JPATH_SCREENSHOTS . $folder . $fileName);
+				$I->makeScreenshot(JPATH_SCREENSHOTS . $base . $folder . $fileName);
 			}
 		}
 	}
